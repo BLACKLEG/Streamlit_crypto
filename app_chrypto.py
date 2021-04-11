7c3355aad6392ea3e4ccf06c72b7778911fdb85c
B. HEMMARA <64562250+BLACKLEG@users.noreply.github.com>
2021-04-11 18:48:52 +0200

    Add files via upload

--- app_chrypto.py
+++ app_chrypto.py
@@ -10,67 +10,151 @@ from bs4 import BeautifulSoup
 import requests
 import json
 import time
+from PIL import Image
 
 
+#---------------------------------------------------------------------------------------------------
+# Page layout (continued)
+## Divide page to 3 columns (col1 = sidebar, col2 and col3 = page contents)
+col1 = st.sidebar
+col2, col3 = st.beta_columns((2,1))
 
+st.markdown(
+    """
+<style>
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#add8e6,#add8e6);
+    color: white;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
 
+# image = Image.open('logo.png')
 
-st.title('Comparateur crypto currency')
+# col3.image(image, width = 150)
 
-
-#---------------------------------#
+# col2.title('Crypto-Application')
+# col2.markdown('On met la daronne a l abris')
+# col2.markdown('''
+# ''')
+# col2.markdown('''
+# ''')
+# col3.markdown('''
+# ''')
+# col3.markdown('''
+# ''')
+# col3.markdown('''
+# ''')
+# col3.markdown('''
+# ''')
+# col3.markdown('''
+# ''')
+# col3.markdown('''
+# ''')
+#---------------------------------------------------------------------------------------------------
 # About
-expander_bar = st.beta_expander("About")
+expander_bar = col1.beta_expander("About")
 expander_bar.markdown("""
 **Source:** 
 * CoinMarketCap (http://coinmarketcap.com) 
 * Yahoo (https://fr.finance.yahoo.com/screener?lang=fr-FR&region=FR) 
 """)
 
-st.title ('#')
 
 
-#---------------------------------#
-# Page layout (continued)
-## Divide page to 3 columns (col1 = sidebar, col2 and col3 = page contents)
-col1 = st.sidebar
-col2, col3 , col4 = st.beta_columns((3,2,1))
+
+#---------------------------------------------------------------------------------------------------
+# tweet coinmarket
+# @st.cache
+# def load_hash():
+#     cmc = requests.get('https://coinmarketcap.com')
+#     soup = BeautifulSoup(cmc.content, 'html.parser')
+#     elements = [] # On crée une liste vide qui contiendra tous les titres propres
+#     for element in soup.findAll(name = 'h1' ,  attrs = {'class' : "sc-fzqBZW iVccbF"}):
+#         elements.append(element.text)
+#     for element in soup.findAll(name = 'div' ,  attrs = {'class' : "sc-AxhCb keRptt"}):
+#         elements.append(element.text)
+#     for element in soup.findAll(name = 'div' ,  attrs = {'class' : "sc-fznBMq ekttti"}):
+#         elements.append(element.text)
+#     return elements
 
 
-#---------------------------------#
-# tweet
-cmc = requests.get('https://coinmarketcap.com')
+# col2.title (''' Hashtag (coinmarketcap)
+# ''')
+# col3.markdown ('''
 
-soup = BeautifulSoup(cmc.content, 'html.parser')
-
-elements = [] # On crée une liste vide qui contiendra tous les titres propres
-
-for element in soup.findAll(name = 'h1' ,  attrs = {'class' : "sc-fzqBZW iVccbF"}):
-    elements.append(element.text)
-    
-for element in soup.findAll(name = 'div' ,  attrs = {'class' : "sc-AxhCb keRptt"}):
-    elements.append(element.text)
-    
-for element in soup.findAll(name = 'div' ,  attrs = {'class' : "sc-fznBMq ekttti"}):
-    elements.append(element.text)
-    
-print(elements)
+# ''')
+# elements = load_hash()
+# col2.warning(elements[2])
+# col2.warning(elements[3])
+# col3.warning(elements[4])
+# col3.success(elements[1] )
 
 
-col2.info(elements[2])
-col3.info(elements[3])
-col4.info(elements[4])
-st.markdown(elements[0])
-st.success(elements[1] )
 
-#---------------------------------#
-# Sidebar + Main panel
-col1.header('Sidebar')
+
+#---------------------------------------------------------------------------------------------------
+# api Yahoo :
+col2.markdown('''
+
+''')
+
+@st.cache
+def get_yahoo_courbe ():
+    # scapping des ticket et des nom
+    cmc = requests.get('https://finance.yahoo.com/cryptocurrencies/?count=100&offset=0')
+    soup = BeautifulSoup(cmc.content, 'html.parser')
+    symbole = [] 
+    nom_crypto = []
+    #page 1 yahoo
+    for element in soup.findAll(name = 'a' ,  attrs = {'class' : "Fw(600) C($linkColor)"}):
+        symbole.append(element.text)
+
+    for element in soup.findAll(name = 'td' ,  attrs = {'class' : "Va(m) Ta(start) Px(10px) Fz(s)"}):
+        nom_crypto.append(element.text)
+    #page 2 yahoo
+    cmc = requests.get('https://finance.yahoo.com/cryptocurrencies/?count=100&offset=100')
+    soup = BeautifulSoup(cmc.content, 'html.parser')
+
+    for element in soup.findAll(name = 'a' ,  attrs = {'class' : "Fw(600) C($linkColor)"}):
+        symbole.append(element.text)
+
+    for element in soup.findAll(name = 'td' ,  attrs = {'class' : "Va(m) Ta(start) Px(10px) Fz(s)"}):
+        nom_crypto.append(element.text)
+    return symbole,nom_crypto
+
+
+#Cytpto monnaies
+ticket_crypto , company_crypto = get_yahoo_courbe ()
+
+deb = '2012-01-01'
+fin = str( '{}-{}-{}'.format(datetime.now().year, datetime.now().month,datetime.now().day) )
+liste_temp = [ 7  , 14  , 30  ]
+
+col1.title ('Param Yahoo')
+place_boursiere_crypto = col1.selectbox(label = "Crypto choice", options = ticket_crypto, key=13)            
+selection_temps = col1.selectbox(label = "Number of days", options = liste_temp, key=15)            
+
+try:
+    df_crypto = dr.data.get_data_yahoo (place_boursiere_crypto , start = deb, end = fin)
+    titre_intro = place_boursiere_crypto
+    # ticket_index = place_boursiere_crypto.index ()
+except:
+    st.error("Problème de récupération des données")
+    st.stop()
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------
+# Web scraping of CoinMarketCap data
 
 ## Sidebar - Currency price unit
-currency_price_unit = col1.selectbox('Select currency for price', ('USD', 'BTC', 'ETH'))
-
-# Web scraping of CoinMarketCap data
+# currency_price_unit = col1.selectbox('Select currency for price', ('USD', 'BTC', 'ETH'))
+currency_price_unit ='USD'
 @st.cache
 def load_data():
     cmc = requests.get('https://coinmarketcap.com')
@@ -117,15 +201,16 @@ df = load_data()
 
 ## Sidebar - Cryptocurrency selections
 sorted_coin = sorted( df['coin_symbol'] )
-selected_coin = col1.multiselect('Cryptocurrency', sorted_coin, sorted_coin)
-
-df_selected_coin = df[ (df['coin_symbol'].isin(selected_coin)) ] # Filtering data
+# selected_coin = col1.multiselect('Cryptocurrency', sorted_coin, sorted_coin)
+# df_selected_coin = df[ (df['coin_symbol'].isin(selected_coin)) ] # Filtering data
+df_selected_coin = df
 
 ## Sidebar - Number of coins to display
-num_coin = col1.slider('Display Top N Coins', 1, 100, 100)
-df_coins = df_selected_coin[:num_coin]
+# num_coin = col1.slider('Display Top N Coins', 1, 100, 100)
+df_coins = df_selected_coin[:]
 
 ## Sidebar - Percent change timeframe
+col1.title ('Param Coinmarketcap')
 percent_timeframe = col1.selectbox('Percent change time frame',
                                     ['1h' , '24h','7d' ])
 
@@ -134,11 +219,8 @@ percent_dict = {"7d":'percentChange7d',"24h":'percentChange24h',"1h":'percentCha
 selected_percent_timeframe = percent_dict[percent_timeframe]
 
 ## Sidebar - Sorting values
-sort_values = col1.selectbox('Sort values?', ['Yes', 'No'])
-
-st.header('Prix des Cryptocurrency')
-st.dataframe(df_coins [['coin_name' , 'coin_symbol' , 'price'  ]] 
-  )
+# sort_values = col2.selectbox('Sort values?', ['Yes', 'No'])
+sort_values = 'Yes'
 # Download CSV data
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
@@ -147,78 +229,65 @@ def filedownload(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="crypto.csv">Download CSV File</a>'
     return href
 
-# st.markdown(filedownload(df_selected_coin), unsafe_allow_html=True)
+col2.title ('Today Cryptocurrency Prices by Market Cap')
+# col2.dataframe(df_coins [['coin_name' , 'coin_symbol'  ]].set_index ('coin_symbol' ) )
+col2.markdown('**Evolution sur 24h**')
 
+col2.dataframe(df_coins.set_index('coin_symbol') [[ 'coin_name' , 'price' , 
+'percentChange24h' ] ].rename ( columns = {  'percentChange1h' : '%1h' , 'percentChange24h' : '%24h' , 'percentChange7d' : '%7d'  }) .sort_values ( by = '%24h' , ascending = False) )
 
+df_1h = df_coins.sort_values ( by = 'percentChange1h' , ascending = False).head(8)
+df_7d = df_coins.sort_values ( by = 'percentChange7d' , ascending = False).head(8)
 
-#---------------------------------#
-# api Yahoo :
+col3.markdown('**Evolution sur 1 heure**')
+col3.dataframe ( df_1h.set_index('coin_symbol')  .rename ( columns = {  'percentChange1h' : '%1h' } ) ['%1h'] )
 
-st.header('Courbes Cryptocurrency')
+col3.markdown('**Evolution sur 7 jours**')
+col3.dataframe ( df_7d.set_index('coin_symbol')  .rename ( columns = {  'percentChange7d' : '%7d' } ) ['%7d'] )
 
+col2.markdown ('''
+''')
+col2.markdown ('''
+''')
+col2.markdown ('''
+''')
+col2.markdown ('''
+''')
+col2.markdown ('''
+''')
+col2.markdown ('''
+''')
+col2.markdown ('''
+''')
+#---------------------------------------------------------------------------------------------------
+# courbe Yahoo 
+col2.title('Courbes Cryptocurrency (Yahoo)')
 
-@st.cache
-def get_yahoo_courbe ():
-    # scapping des ticket et des nom
-    cmc = requests.get('https://finance.yahoo.com/cryptocurrencies/?count=100&offset=0')
-    soup = BeautifulSoup(cmc.content, 'html.parser')
-    symbole = [] 
-    nom_crypto = []
-    #page 1 yahoo
-    for element in soup.findAll(name = 'a' ,  attrs = {'class' : "Fw(600) C($linkColor)"}):
-        symbole.append(element.text)
-
-    for element in soup.findAll(name = 'td' ,  attrs = {'class' : "Va(m) Ta(start) Px(10px) Fz(s)"}):
-        nom_crypto.append(element.text)
-    #page 2 yahoo
-    cmc = requests.get('https://finance.yahoo.com/cryptocurrencies/?count=100&offset=100')
-    soup = BeautifulSoup(cmc.content, 'html.parser')
-
-    for element in soup.findAll(name = 'a' ,  attrs = {'class' : "Fw(600) C($linkColor)"}):
-        symbole.append(element.text)
-
-    for element in soup.findAll(name = 'td' ,  attrs = {'class' : "Va(m) Ta(start) Px(10px) Fz(s)"}):
-        nom_crypto.append(element.text)
-    return symbole,nom_crypto
-
-
-#Cytpto monnaies
-ticket_crypto , company_crypto = get_yahoo_courbe ()
-
-deb = '2012-01-01'
-fin = str( '{}-{}-{}'.format(datetime.now().year, datetime.now().month,datetime.now().day) )
-liste_temp = [ 7  , 14  , 30  ]
-
-place_boursiere_crypto = col1.selectbox(label = "Courbe - Choice of crypto", options = ticket_crypto, key=13)            
-selection_temps = col1.selectbox(label = "Courbe - Number of days", options = liste_temp, key=15)            
-
-try:
-    df_crypto = dr.data.get_data_yahoo (place_boursiere_crypto , start = deb, end = fin)
-    titre_intro = place_boursiere_crypto
-    # ticket_index = place_boursiere_crypto.index ()
-except:
-    st.error("Problème de récupération des données")
-    st.stop()
-
-
-#-------------------------------------- courbe
 plotly_figure_intro_crypto = px.line(data_frame = df_crypto, 
                         x = df_crypto.index, 
                         y = ['Close' ] , 
-                        title = titre_intro + ' - Prix de la Crypto' )
-st.plotly_chart ( plotly_figure_intro_crypto)
+                        title = titre_intro + ' - Cours' )
+col2.plotly_chart ( plotly_figure_intro_crypto)
 
 df_crypto_reduced = df_crypto.tail (selection_temps)
 plotly_figure_intro_crypto_r = px.line(data_frame =df_crypto_reduced, 
                         x = df_crypto_reduced.index, 
                         y = ['Close' ] , 
                         title = titre_intro + ' - Last ' + str (selection_temps)+ ' days' )
-st.plotly_chart ( plotly_figure_intro_crypto_r)
+col2.plotly_chart ( plotly_figure_intro_crypto_r)
 
 
 
-#---------------------------------#
-# Bar plot evolution du prix :
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------------------------------
+#  Bar plot evolution du prix :
+col2.title('Evolution du prix (%) - Coinmarketcap')
 
 # st.header('Table of % Price Change')
 df_change = pd.concat([df_coins.coin_symbol, df_coins.percentChange1h, df_coins.percentChange24h, df_coins.percentChange7d], axis=1)
@@ -229,30 +298,47 @@ df_change['positive_percent_change_7d'] = df_change['percentChange7d'] > 0
 # st.dataframe(df_change)
 
 # # Conditional creation of Bar plot (time frame)
-st.header('Evolution du prix (%) - barplot')
 
 if percent_timeframe == '7d':
     if sort_values == 'Yes':
         df_change = df_change.sort_values(by=['percentChange7d'])
-    st.write('*7 days period*')
+    col2.write('*7 days period*')
     plt.figure(figsize=(5,25))
     plt.subplots_adjust(top = 1, bottom = 0)
     df_change['percentChange7d'].plot(kind='barh', color=df_change.positive_percent_change_7d.map({True: 'g', False: 'r'}))
-    st.pyplot(plt)
+    col2.pyplot(plt)
 elif percent_timeframe == '24h':
     if sort_values == 'Yes':
         df_change = df_change.sort_values(by=['percentChange24h'])
-    st.write('*24 hour period*')
+    col2.write('*24 hour period*')
     plt.figure(figsize=(5,25))
     plt.subplots_adjust(top = 1, bottom = 0)
     df_change['percentChange24h'].plot(kind='barh', color=df_change.positive_percent_change_24h.map({True: 'g', False: 'r'}))
-    st.pyplot(plt)
+    col2.pyplot(plt)
 else:
     if sort_values == 'Yes':
         df_change = df_change.sort_values(by=['percentChange1h'])
-    st.write('*1 hour period*')
+    col2.write('*1 hour period*')
     plt.figure(figsize=(5,25))
     plt.subplots_adjust(top = 1, bottom = 0)
     df_change['percentChange1h'].plot(kind='barh', color=df_change.positive_percent_change_1h.map({True: 'g', False: 'r'}))
-    st.pyplot(plt)
+    col2.pyplot(plt)
+
+col1.title ('Dictionnaire')
+col1.dataframe(df_coins [['coin_name' , 'coin_symbol'  ]].set_index ('coin_symbol' ).sort_index() )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
